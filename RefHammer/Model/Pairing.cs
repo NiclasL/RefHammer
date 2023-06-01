a3708aac9e315503cc97e0de6300d66a3d6c3dd5
Kolosan <NiclasLarsenn@gmail.com>
2023-06-02 01:37:21 +0200

    Added Tournament Base

---
+++ RefHammer/Model/Pairing.cs
@@ -0,0 +1,14 @@
+﻿namespace RefHammer.Model
+{
+    public class Pairing
+    {
+        public Participant Player1 { get; set; }
+        public Participant Player2 { get; set; }
+
+        public Pairing(Participant player1, Participant player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+        }
+    }
+}
---
+++ RefHammer/Model/Round.cs
@@ -0,0 +1,19 @@
+﻿namespace RefHammer.Model
+{
+    public class Round
+    {
+        public int Number { get; set; }
+        public List<Pairing> Pairings { get; set; }
+
+        public Round(int number)
+        {
+            Number = number;
+            Pairings = new List<Pairing>();
+        }
+
+        public void AddPairing(Pairing pairing)
+        {
+            Pairings.Add(pairing);
+        }
+    }
+}
